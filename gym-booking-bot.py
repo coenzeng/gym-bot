3f657fe96e783de96222d850bd8ddef12eb1722b
coenzeng <63679316+coenzeng@users.noreply.github.com>
2021-05-13 15:59:10 -0400

    allow command line parameters

--- gym-booking-bot.py
+++ gym-booking-bot.py
@@ -1,3 +1,6 @@
+#run command:
+#python gym-booking-bot.py email password
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,15 +9,18 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 import datetime
 import os
+import sys
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
+#print(os.environ.get("CHROMEDRIVER_PATH"))
+#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
+#function that scrolls to elements in the web page
 def scrollTo(driver, element):
     driver.execute_script("""arguments[0].scrollIntoView({
             block: 'center',
@@ -23,22 +29,27 @@ def scrollTo(driver, element):
     driver.execute_script("arguments[0].scrollIntoView();", element);
     return element
 
-#PATH = "C:\Program Files (x86)\selenium\chromedriver.exe"
-#driver = webdriver.Chrome(PATH)
+class Person():
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+user = Person(sys.argv[1], sys.argv[2])
+
+#path to chrome driver
+PATH = "C:\Program Files (x86)\selenium\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 
 driver.get("https://myfit4less.gymmanager.com/portal/login.asp")
 
-
 email = scrollTo(driver, driver.find_element_by_id('emailaddress'))
 
-#ENTER EMAIL BETWEEN ''
-email.send_keys("")
-
+#ENTER EMAIL
+email.send_keys(user.email)
 pw = scrollTo(driver, driver.find_element_by_id('password'))
 
-#ENTER PASSWORD BETWEEN ''
-pw.send_keys("")
-
+#ENTER PASSWORD 
+pw.send_keys(user.password)
 login=scrollTo(driver, driver.find_element_by_id('loginButton'))     
 login.click()
 
@@ -46,13 +57,20 @@ login.click()
 #scrollTo(driver, driver.find_element_by_id('btn_club_select')).click()
 #scrollTo(driver, driver.find_element_by_id('club_0E64CA8C-388B-4CD8-B1D7-385369A15E7B')).click()   
 #   
+
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 dayaftertomorrow = (today + datetime.timedelta(days=2)).strftime("%Y-%m-%d")
 three_days_later = (today + datetime.timedelta(days=3)).strftime("%Y-%m-%d")
 
-select_day=scrollTo(driver, driver.find_element_by_id('btn_date_select'))     
-select_day.click()
+try:
+    select_day=scrollTo(driver, driver.find_element_by_id('btn_date_select'))   
+    select_day.click()
+except NoSuchElementException:
+    print("Gyms are closed!")  
+    sys.exit()
+
+
 
 try:
     driver.find_element_by_id("date_" + three_days_later).click()
